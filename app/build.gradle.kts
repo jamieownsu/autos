86829ebdf44532034d1aae0e762daaad9885d356
Jamie <jamie.a.otoole@gmail.com>
2025-07-27 19:02:01 -0400

    -ui tests for fun

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         versionCode = 1
         versionName = "1.0"
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        testInstrumentationRunner = "com.chalupin.carfax.HiltTestRunner"
     }
 
     buildTypes {
@@ -89,6 +89,7 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
+    androidTestImplementation(libs.hilt.android.testing)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 }
--- app/src/main/java/com/chalupin/carfax/core/theme/Theme.kt
+++ app/src/main/java/com/chalupin/carfax/core/theme/Theme.kt
@@ -8,17 +8,16 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Color(0xFF336699),
+    primary = CarfaxBlueButBluer,
     secondary = PurpleGrey80,
     tertiary = Pink80
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Color(0xFF336699),
+    primary = CarfaxBlue,
     secondary = PurpleGrey40,
     tertiary = Pink40,
 //    background = Color(0xFFFFFBFE),
@@ -33,7 +32,6 @@ private val LightColorScheme = lightColorScheme(
 @Composable
 fun CarFaxTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
     dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
